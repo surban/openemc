bd8b505f760aa632bc6b9426cade075760621655
Sebastian Urban <surban@surban.net>
2023-07-10 20:42:23 +0200

    Add standby function to bootlaoder

--- openemc-bootloader/src/util.rs
+++ openemc-bootloader/src/util.rs
@@ -1,4 +1,7 @@
-//! Debug functions.
+//! Utility functions.
+
+use cortex_m::{asm::wfi, peripheral::SCB};
+use stm32f1::stm32f103::Peripherals;
 
 use crate::board::Board;
 
@@ -6,4 +9,49 @@ use crate::board::Board;
 pub fn delay_ms(ms: u32) {
     let cycles = ms * (crate::boards::Chosen::CPU_CLOCK / 1000);
     cortex_m::asm::delay(cycles)
+}
+
+/// Enters standby mode.
+#[allow(dead_code)]
+pub fn enter_standby() -> ! {
+    let mut cp = unsafe { cortex_m::Peripherals::steal() };
+    let dp = unsafe { Peripherals::steal() };
+
+    // Check if WKUP pin is low.
+    dp.GPIOA.crl.modify(|_, w| w.cnf0().open_drain().mode0().input());
+    let wkup = dp.GPIOA.idr.read().idr0();
+    if wkup.is_high() {
+        // Device should stay awake, convert shutdown into reset.
+        defmt::info!("WKUP pin is high, turning shutdown into reset");
+        delay_ms(10);
+        SCB::sys_reset();
+    }
+
+    // Disable debug workarounds.
+    //dp.DBGMCU.cr.modify(|_, w| w.dbg_sleep().clear_bit().dbg_stop().clear_bit().dbg_standby().clear_bit());
+
+    // Disable and clear SysTick interrupt.
+    cp.SYST.disable_interrupt();
+    cp.SYST.disable_counter();
+    SCB::clear_pendst();
+
+    // Disable and clear all interrupts.
+    for icer in cp.NVIC.icer.iter() {
+        unsafe { icer.write(0xffffffff) };
+    }
+    for icpr in cp.NVIC.icpr.iter() {
+        unsafe { icpr.write(0xffffffff) };
+    }
+
+    // Enable standby mode and wake up using WKUP pin.
+    dp.PWR.csr.modify(|_, w| w.ewup().set_bit());
+    dp.PWR.cr.modify(|_, w| w.lpds().set_bit().cwuf().set_bit().pdds().standby_mode());
+    cp.SCB.set_sleepdeep();
+
+    // Enter standby mode.
+    defmt::warn!("entering standby mode");
+    delay_ms(10);
+    loop {
+        wfi()
+    }
 }
