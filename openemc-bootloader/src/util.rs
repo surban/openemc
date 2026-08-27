2b12fb67655f36afbce432065e52c83c5d5fa441
Sebastian Urban <surban@surban.net>
2026-08-27 22:45:03 +0200

    Update dependencies
    Requires adjustments of timing loop.

--- openemc-bootloader/src/util.rs
+++ openemc-bootloader/src/util.rs
@@ -7,7 +7,7 @@ use crate::board::Board;
 
 /// Approximately delay execution by specified amount of milliseconds.
 pub fn delay_ms(ms: u32) {
-    let cycles = ms * (crate::boards::Chosen::CPU_CLOCK / 1000);
+    let cycles = ms * (crate::boards::Chosen::CPU_CLOCK / 1000) / 3;
     cortex_m::asm::delay(cycles)
 }
 
